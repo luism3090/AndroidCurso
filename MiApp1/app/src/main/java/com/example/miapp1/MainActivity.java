8bd8d720924f7112cd55d10c14e396a60601ef22
Luis Molina <luisame@outlook.com>
2020-02-21 12:13:41 -0600

    Subiendo el Hola mundo

---
+++ MiApp1/app/src/main/java/com/example/miapp1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.miapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Hola Mundo",Toast.LENGTH_SHORT ).show();
+
+    }
+}
