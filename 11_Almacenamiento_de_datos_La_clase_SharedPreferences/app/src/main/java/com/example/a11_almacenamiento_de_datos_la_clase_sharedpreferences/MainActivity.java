b9205adbc5a0a86c08fedb58dbbf7689b8938057
Luis Molina <luisame@outlook.com>
2020-02-28 12:47:43 -0600

    Practica finalizada alamacenamiento de datos clase SharedPreferences

--- 11_Almacenamiento_de_datos_La_clase_SharedPreferences/app/src/main/java/com/example/a11_almacenamiento_de_datos_la_clase_sharedpreferences/MainActivity.java
+++ 11_Almacenamiento_de_datos_La_clase_SharedPreferences/app/src/main/java/com/example/a11_almacenamiento_de_datos_la_clase_sharedpreferences/MainActivity.java
@@ -20,15 +20,34 @@ public class MainActivity extends AppCompatActivity {
 
         txt_email = (EditText)findViewById(R.id.txt_email);
 
-        SharedPreferences mipreferencia = getSharedPreferences("datos", Context.MODE_PRIVATE);
 
-        txt_email.setText(mipreferencia.getString("mail",""));
+        // codigo para indicarle a android que invocamos un archivo llamado datos de la clase SharedPreferences
+        SharedPreferences mipreferencia1 = getSharedPreferences("datos", Context.MODE_PRIVATE);
+
+
+        // codigo para obtener el valor guardado en sharedpreferences  y colocarlo en la caja de texto
+        txt_email.setText(mipreferencia1.getString("mail",""));
 
     }
 
     public  void guardar(View v){
 
+        // codigo para indicarle a android que invocamos un archivo llamado datos de la clase SharedPreferences
+        SharedPreferences mipreferencia2 = getSharedPreferences("datos", Context.MODE_PRIVATE);
 
+        // codigo para poder editar el archivo datos de la clase SharedPreferences
+        SharedPreferences.Editor obj_editor  = mipreferencia2.edit();
+
+        // codigo para indicarle el dato a guardar del archivo datos de la clase SharedPreferences
+        // el dato a guardar se almacenará en el atributo mail con el valor que contenga la caja de texto mail
+        obj_editor.putString("mail",txt_email.getText().toString());
+
+
+        // Y por ultimo debemos usar el metodo commit para confirmar la informacion a guardar
+        obj_editor.commit();
+
+        // codigo para indicarle a android que la activity se cierre
+        finish();
 
     }
 
