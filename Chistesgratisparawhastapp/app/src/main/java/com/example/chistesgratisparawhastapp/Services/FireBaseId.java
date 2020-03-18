90a89dc4c3140cc12691fa04dcda2ff0e59cb3fe
Luis Molina <luisame@outlook.com>
2020-03-17 18:01:23 -0600

    acabando de configurar FireBase en android studio

---
+++ Chistesgratisparawhastapp/app/src/main/java/com/example/chistesgratisparawhastapp/Services/FireBaseId.java
@@ -0,0 +1,10 @@
+package com.example.chistesgratisparawhastapp.Services;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FireBaseId extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+    }
+}
---
+++ Chistesgratisparawhastapp/app/src/main/java/com/example/chistesgratisparawhastapp/Services/FireBaseServiceMensajes.java
@@ -0,0 +1,11 @@
+package com.example.chistesgratisparawhastapp.Services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FireBaseServiceMensajes extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
